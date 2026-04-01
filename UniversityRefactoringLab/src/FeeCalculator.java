3ba5b180e619c2c969278ce70a5cc7bb58737afe
Rida Hashmi <rhc633@uregina.ca>
2026-03-31 22:35:04 -0600

    Extract fee calculation into FeeCalculator class

--- UniversityRefactoringLab/src/FeeCalculator.java
+++ UniversityRefactoringLab/src/FeeCalculator.java
@@ -1,2 +1,40 @@
 public class FeeCalculator {
+
+    public double calculateFee(Student student, Course course, String semester, String paymentType) {
+        double fee = 0;
+
+        StudentType studentType = StudentType.valueOf(student.type);
+
+        if (studentType == StudentType.LOCAL) {
+            fee = course.creditHours * 300;
+        } else if (studentType == StudentType.INTERNATIONAL) {
+            fee = course.creditHours * 550;
+        } else if (studentType == StudentType.SCHOLARSHIP) {
+            fee = course.creditHours * 100;
+        } else {
+            fee = course.creditHours * 300;
+        }
+
+        PaymentMethod paymentMethod = PaymentMethod.valueOf(paymentType);
+
+        if (paymentMethod == PaymentMethod.INSTALLMENT) {
+            fee = fee + 50;
+        } else if (paymentMethod == PaymentMethod.CARD) {
+            fee = fee + 10;
+        } else if (paymentMethod == PaymentMethod.CASH) {
+            fee = fee + 0;
+        } else {
+            fee = fee + 100;
+        }
+
+        if (semester.equals("SUMMER")) {
+            fee = fee + 200;
+        }
+
+        if (course.code.startsWith("SE")) {
+            fee = fee + 75;
+        }
+
+        return fee;
+    }
 }
--- UniversityRefactoringLab/src/UniversitySystem.java
+++ UniversityRefactoringLab/src/UniversitySystem.java
@@ -89,7 +89,8 @@ public class UniversitySystem {
             }
         }
 
-        double fee = calculateFee(student, course, semester, paymentType);
+        FeeCalculator feeCalculator = new FeeCalculator();
+        double fee = feeCalculator.calculateFee(student, course, semester, paymentType);
         StudentType studentType = StudentType.valueOf(student.type);
 
         student.outstandingBalance = student.outstandingBalance + fee;
@@ -111,41 +112,6 @@ public class UniversitySystem {
             System.out.println("Invalid email");
             logs.add("Invalid email for " + student.id);
         }
-    }
-
-    private double calculateFee(Student student, Course course, String semester, String paymentType) {
-        double fee = 0;
-        StudentType studentType = StudentType.valueOf(student.type);
-
-        if (studentType == StudentType.LOCAL) {
-            fee = course.creditHours * 300;
-        } else if (studentType == StudentType.INTERNATIONAL) {
-            fee = course.creditHours * 550;
-        } else if (studentType == StudentType.SCHOLARSHIP) {
-            fee = course.creditHours * 100;
-        } else {
-            fee = course.creditHours * 300;
-        }
-
-        PaymentMethod paymentMethod = PaymentMethod.valueOf(paymentType);
-        if (paymentMethod == PaymentMethod.INSTALLMENT) {
-            fee = fee + 50;
-        } else if (paymentMethod == PaymentMethod.CARD) {
-            fee = fee + 10;
-        } else if (paymentMethod == PaymentMethod.CASH) {
-            fee = fee + 0;
-        } else {
-            fee = fee + 100;
-        }
-
-        if (semester.equals("SUMMER")) {
-            fee = fee + 200;
-        }
-
-        if (course.code.startsWith("SE")) {
-            fee = fee + 75;
-        }
-        return fee;
     }
 
     public void assignGrade(String studentId, String courseCode, String semester, String grade) {
